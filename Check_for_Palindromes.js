3713903b270ae1700aec1c9dc6ff563afe415ec3
jennift <jennift1@yahoo.com>
2017-11-06 15:42:13 +1100

    Add existing file

---
+++ Check_for_Palindromes.js
@@ -0,0 +1,22 @@
+var startString = "";
+var endString = "";
+
+function palindrome(str) {
+  
+  startString = str.toLowerCase();
+  startString = startString.replace(/[^a-zA-Z0-9]/gi,"");
+  
+  endString = startString.split("");
+  endString = endString.reverse();
+  endString = endString.join("");
+  //return startString + "\n" + endString;
+  
+  if (startString == endString) {
+    return true;
+  } else {
+    return false;
+  }
+  
+}
+
+palindrome("_eye");
